ab1db5d5d62edb171f8d34717f2154839eabbb31
Liliana Meriweather <liliana.meriweather@tlgcohort.com>
2023-03-14 12:07:48 -0700

    Ant Files Added, changes to Ant, App, and Board.

--- src/com/buildaant/Board.java
+++ src/com/buildaant/Board.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
@@ -12,7 +14,7 @@ public class Board {
     private static final int NUM_BODY_PARTS = 6;
 
 
-    static Board getInstanceOf(){
+    public static Board getInstanceOf(){
         Board board = null;
 
         if(Files.exists(Path.of("data/board.dat"))){
@@ -29,37 +31,36 @@ public class Board {
         return board;
     }
 
+    private final Map<Integer, AntPiece> antPieceMap = new HashMap<>(); // (diceRoll, antPiece)
+    private final Map<Player, List<AntPiece>> playerPieces = new HashMap<>(); // (Player, player pieces)
 
-//    private final Map<Integer, AntPiece> antPieceMap = HashMap<>();
 
-    public void update(){   //update to the perspective players board
+//    public void update(Ant ant, Player player, Player Player2){   //update to the perspective players board
+//        printBoards(, );
+//        printBoard();
+//    }
 
-    }
 
-    public void printBuiltParts(Ant ant, Player player1, Player player2) {
-        System.out.println("Body parts built:");
-        for (AntPiece part : AntPiece.values()) {
-            boolean player1HasPart = ant.hasPart(part, player1);
-            boolean player2HasPart = ant.hasPart(part, player2);
-            String partName = part.toString();
-            String player1Status = player1HasPart ? "built" : "not built";
-            String player2Status = player2HasPart ? "built" : "not built";
-            System.out.println(partName + ": Player 1 " + player1Status + ", Player 2 " + player2Status);
-        }
-    }
-    public void printBoards(Ant ant, Player player1, Player player2) {
+    public void printBoards() {
+        System.out.println("            BUILD-A-ANT               ");
+        System.out.println("======================================\n");
+
         System.out.println("Player 1 board:");
-        printBoard(ant, player1);
+        System.out.println("-----------------");
+        //printBoard();
+
+
         System.out.println("Player 2 board:");
-        printBoard(ant, player2);
+        System.out.println("----------------");
+        //printBoard();
+
+        Collection<List<AntPiece>> allPieces = playerPieces.values(); // return collection of pieces?
+
+        for (List<AntPiece> antPiece : allPieces) {
+            System.out.printf("%s            %s             %s \n",
+                    antPiece.get(1), antPiece.get(2), antPiece.get(3));
+        }
+
     }
 
-    private void printBoard(Ant ant, Player player) {
-        System.out.println("Body: " + (ant.hasPart(AntPiece.BODY, player) ? "built" : "not built"));
-        System.out.println("Head: " + (ant.hasPart(AntPiece.HEAD, player) ? "built" : "not built"));
-        System.out.println("Legs: " + (ant.hasPart(AntPiece.LEGS, player) ? "built" : "not built"));
-        System.out.println("Tail: " + (ant.hasPart(AntPiece.TAIL, player) ? "built" : "not built"));
-        System.out.println("Eyes: " + (ant.hasPart(AntPiece.EYE, player) ? "built" : "not built"));
-        System.out.println("Antennae: " + (ant.hasPart(AntPiece.ANTENNA, player) ? "built" : "not built"));
-    }
 }
