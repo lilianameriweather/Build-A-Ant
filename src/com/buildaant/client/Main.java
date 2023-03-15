de0543db1f11e1e5a1c9e0a3b4a189ff153797ee
Chance Jolley <chance.jolley@tlgcohort.com>
2023-03-15 12:57:33 -0700

    Worked on App and Main

--- src/com/buildaant/client/Main.java
+++ src/com/buildaant/client/Main.java
@@ -1,11 +1,14 @@
 package com.buildaant.client;
 
+import com.apps.util.Prompter;
 import com.buildaant.app.BuildAAntApp;
+
+import java.util.Scanner;
 
 class Main {
 
     public static void main(String[] args) {
-        BuildAAntApp app = new BuildAAntApp();
+        BuildAAntApp app = new BuildAAntApp(new Prompter(new Scanner(System.in)));
         app.execute();
 
     }
