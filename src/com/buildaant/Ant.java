1b6a059fc022323d6162d50240862a50b72a0dfb
Liliana Meriweather <liliana.meriweather@tlgcohort.com>
2023-03-15 09:26:54 -0700

    Saving Ant Add method

--- src/com/buildaant/Ant.java
+++ src/com/buildaant/Ant.java
@@ -10,6 +10,7 @@ public class Ant {
     private boolean hasHead = false;
     private boolean hasBody = false;
 
+    /*
     public void add(AntPiece piece) throws NotPossibleException {
 
         if(!hasHead && !hasBody) {
@@ -34,7 +35,46 @@ public class Ant {
             }
         }
 
+    } */
+
+    public void add(AntPiece piece) throws NotPossibleException {
+        // --------------------HEAD-BODY-------------------------
+        if (hasPiece(piece)) {
+            throw new NotPossibleException("You Already have this piece: " + piece);
+        }
+        if (AntPiece.HEAD == piece) {
+            pieces.add(piece);
+            hasHead = true;
+        }
+        else if (AntPiece.BODY == piece) {
+            pieces.add(piece);
+            hasBody = true;
+        }
+        //-----------------4--possible-options-----------------------
+        else if (hasHead && hasBody) {
+            pieces.add(piece);
+        }
+        else if (hasHead) {
+            if (AntPiece.ANTENNA == piece || AntPiece.EYES == piece) {
+                pieces.add(piece);
+            }
+            else {
+                throw new NotPossibleException("Only antenna and eyes can be added to head.");
+            }
+        }
+        else if (hasBody) {
+            if (AntPiece.LEGS == piece || AntPiece.TAIL == piece) {
+                pieces.add(piece);
+            }
+            else {
+                throw new NotPossibleException("Only legs and tail can be added to body.");
+            }
+        }
+        else {
+            throw new NotPossibleException("Must have head or body first");
+        }
     }
+
 
     public void show() {
 
